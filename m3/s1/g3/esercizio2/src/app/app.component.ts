9b224e904c9831a811e7e54a7d657b6ba29537d5
Simone Giarnera <simone.giarnera@gmail.com>
2024-03-20 16:21:43 +0100

    svolgimento esercizio

--- m3/s1/g3/esercizio2/src/app/app.component.ts
+++ m3/s1/g3/esercizio2/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { iArticolo } from './Models/articolo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'esercizio2';
+  articoliArr: iArticolo[] = [];
+  randomArray: iArticolo[] = [];
+
+  ngOnInit() {
+    fetch('../assets/db.json')
+      .then((articoli) => articoli.json())
+      .then((articolo) => {
+        this.articoliArr = articolo.posts;
+        this.generaArrayRandom();
+      });
+  }
+
+  generaNumeroRandom() {
+    let index: number = Math.floor(Math.random() * this.articoliArr.length);
+    return index;
+  }
+
+  generaArrayRandom() {
+    for (let i = 0; i < 4; i++) {
+      this.randomArray.push(this.articoliArr[this.generaNumeroRandom()]);
+    }
+  }
 }
--- m3/s1/g3/esercizio2/src/app/app.module.ts
+++ m3/s1/g3/esercizio2/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
