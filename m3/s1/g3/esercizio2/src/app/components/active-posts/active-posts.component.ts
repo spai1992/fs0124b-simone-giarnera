bf895339698b7bb1ebe5bc5605203ffff2a1c44b
Simone Giarnera <simone.giarnera@gmail.com>
2024-03-21 15:52:22 +0100

    fine esercizio

--- m3/s1/g3/esercizio2/src/app/components/active-posts/active-posts.component.ts
+++ m3/s1/g3/esercizio2/src/app/components/active-posts/active-posts.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ArticoliService } from '../../articoli.service';
+import { iArticolo } from '../../Models/articolo';
 
 @Component({
   selector: 'app-active-posts',
   templateUrl: './active-posts.component.html',
-  styleUrl: './active-posts.component.scss'
+  styleUrl: './active-posts.component.scss',
 })
 export class ActivePostsComponent {
+  articoliArr: iArticolo[] = [];
 
+  constructor(private articoliSvc: ArticoliService) {}
+
+  ngOnInit() {
+    this.articoliSvc.getActiveArticle().then((res) => {
+      this.articoliArr = res;
+    });
+  }
 }
--- m3/s1/g3/esercizio2/src/app/components/inactive-posts/inactive-posts.component.ts
+++ m3/s1/g3/esercizio2/src/app/components/inactive-posts/inactive-posts.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { iArticolo } from '../../Models/articolo';
+import { ArticoliService } from '../../articoli.service';
 
 @Component({
   selector: 'app-inactive-posts',
   templateUrl: './inactive-posts.component.html',
-  styleUrl: './inactive-posts.component.scss'
+  styleUrl: './inactive-posts.component.scss',
 })
 export class InactivePostsComponent {
+  articoliArr: iArticolo[] = [];
 
+  constructor(private articoliSvc: ArticoliService) {}
+
+  ngOnInit() {
+    this.articoliSvc.getInactiveArticle().then((res) => {
+      this.articoliArr = res;
+    });
+  }
 }
