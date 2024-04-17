87bb3eacea2980e85df1904c56653ae5d706f37e
Simone Giarnera <51135928+spai1992@users.noreply.github.com>
2024-04-17 15:42:59 +0200

    creazione esercizio 2

---
+++ Backend/m1/s1/G3/src/pachetto2/java/Main.java
@@ -0,0 +1,14 @@
+package pachetto2.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
---
+++ Backend/m1/s1/G3/src/pachetto2/java/Sim.java
@@ -0,0 +1,18 @@
+package pachetto2.java;
+
+public class Sim {
+
+    int numeroTelefono;
+    double credito;
+    String[] chiamate;
+
+    public Sim(int n) {
+
+        this.numeroTelefono = n;
+        credito = 0.0;
+        chiamate = new String[0];
+
+    }
+
+
+}
