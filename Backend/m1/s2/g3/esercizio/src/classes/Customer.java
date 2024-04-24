a4877e5a4772d33252af22f3a0a76f3ffea4877e
Simone Giarnera <51135928+spai1992@users.noreply.github.com>
2024-04-24 14:30:04 +0200

    g3 esercizio

---
+++ Backend/m1/s2/g3/esercizio/src/classes/Customer.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private Integer tier;
+
+    public Customer(Long id, String name, Integer tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+
+}
